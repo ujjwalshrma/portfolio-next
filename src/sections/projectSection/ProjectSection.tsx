c9d3427a76b402050633febdb9c136444ed6e607
us0610 <ujjwal158b@gmail.com>
2023-05-27 18:39:23 +0530

    added projects section and responsiveness

---
+++ src/sections/projectSection/ProjectSection.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import styles from "./ProjectSection.module.css";
+
+import { useState } from "react";
+
+import { Projects } from "./Projects";
+
+import Image from "next/image";
+import Button from "@/components/button/Button";
+
+function ProjectSection() {
+  const [hoveredImgId, setHoveredImgId] = useState<number>(NaN);
+
+  const handleMouseEnter = (id: number) => {
+    setHoveredImgId(id)
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredImgId(NaN)
+  }
+
+  return (
+    <section id="projects" className={styles.projects}>
+      <div className="container">
+        <div className={styles.projects__wrapper}>
+          {Projects.map((project) => (
+            <div key={project.id} className={styles.project__card}>
+              <div>
+                <Image
+                  className={
+                    project.id === hoveredImgId || window.innerWidth < 768
+                      ? `${styles.project__img} ${styles.hover}`
+                      : styles.project__img
+                  }
+                  src={project.img}
+                  alt={project.title}
+                />
+              </div>
+              <div className={styles.project__text}>
+                <div>
+                  <h2 className={styles.title}>{project.title}</h2>
+                  <p className={styles.description}>{project.description}</p>
+                </div>
+                <div
+                  onMouseEnter={() => handleMouseEnter(project.id)}
+                  onMouseLeave={() => handleMouseLeave()}
+                >
+                  <Button href={project.link}>View More</Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProjectSection;
